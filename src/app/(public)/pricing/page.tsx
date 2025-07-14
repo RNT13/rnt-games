43a108b2e4d0f9e02c4be541865c3fc569c53212
RNT13 <renatornt13@gmail.com>
2025-07-14 17:34:32 -0300

    Update: adição de pricing page

--- src/app/(public)/pricing/page.tsx
+++ src/app/(public)/pricing/page.tsx
@@ -1,9 +1,100 @@
 'use client'
 
+import { Button } from "@/components/ui/Button/Button"
+import { TitleH2 } from "@/styles/globalStyles"
+import { FaCheck } from "react-icons/fa"
+import { ButtonDiv, PricingCardBody, PricingCardHeader, PricingContainer, PricingContent, PricingPlan, PricingPlanCard } from "./pricingStyles"
+
 export default function Pricing() {
   return (
-    <div>
-      <h1>Pricing</h1>
-    </div>
+    <PricingContainer>
+      <PricingContent >
+        <TitleH2>Escolha seu plano</TitleH2>
+        <PricingPlan>
+          <PricingPlanCard className="free">
+            <PricingCardHeader>
+              <TitleH2>GRATUITO</TitleH2>
+              <p>R$ 0 // Mês</p>
+              <p>Não temos tarifas ou cobranças, apenas o que você precisa para começar.</p>
+            </PricingCardHeader>
+            <ButtonDiv>
+              <Button className="freeButton">Seu plano atual</Button>
+            </ButtonDiv>
+            <PricingCardBody>
+              <li>
+                <FaCheck />
+                <p>Acesso ao catalogo sem descontos</p>
+              </li>
+              <li>
+                <FaCheck />
+                <p>Não recebe jogos gratis</p>
+              </li>
+              <li>
+                <FaCheck />
+                <p>Sem acesso ao PlayNuvem</p>
+              </li>
+            </PricingCardBody>
+          </PricingPlanCard>
+          <PricingPlanCard className="premium">
+            <PricingCardHeader>
+              <div>
+                <TitleH2>PREMIUM</TitleH2>
+                <span>Mais popular</span>
+              </div>
+              <p>R$ 4.99 // Mês</p>
+              <p>Descontos exclusivos e sorteios de jogos todo mes</p>
+            </PricingCardHeader>
+            <ButtonDiv>
+              <Button className="premiumButton">Obter plano premium</Button>
+            </ButtonDiv>
+            <PricingCardBody>
+              <li>
+                <FaCheck />
+                <p>Acesso ao catalogo com diversos descontos</p>
+              </li>
+              <li>
+                <FaCheck />
+                <p>Participa de sorteios de jogos todo mês</p>
+              </li>
+              <li>
+                <FaCheck />
+                <p>Pode baixar jogos gratis ilimitados</p>
+              </li>
+              <li>
+                <FaCheck />
+                <p>Acesso limitado ao PlayNuvem</p>
+              </li>
+            </PricingCardBody>
+          </PricingPlanCard>
+          <PricingPlanCard className="pro">
+            <PricingCardHeader>
+              <TitleH2>PRO</TitleH2>
+              <p>R$ 14.99 // Mês</p>
+            </PricingCardHeader>
+            <ButtonDiv>
+              <Button className="proButton">Obter plano PRO</Button>
+            </ButtonDiv>
+            <PricingCardBody>
+              <li>
+                <FaCheck />
+                <p>Acesso ao catalogo com descontos exclusivos</p>
+              </li>
+              <li>
+                <FaCheck />
+                <p>Participa de sorteios de jogos todo semana</p>
+              </li>
+              <li>
+                <FaCheck />
+                <p>Pode baixar jogos pagos e testalos por 3 horas</p>
+              </li>
+              <li>
+                <FaCheck />
+                <p>Acesso ilimitado ao PlayNuvem. Jogue seus jogos onde quiser</p>
+              </li>
+            </PricingCardBody>
+          </PricingPlanCard>
+        </PricingPlan>
+      </PricingContent >
+    </PricingContainer >
   )
 }
---
+++ src/app/(public)/pricing/pricingStyles.ts
@@ -0,0 +1,180 @@
+import { theme, transitions } from '@/styles/theme'
+import { styled } from 'styled-components'
+
+export const PricingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const PricingContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 50px 0;
+  height: 100%;
+  width: 100%;
+
+  > h2 {
+    font-size: 32px;
+    margin: 50px 0;
+  }
+`
+
+export const PricingPlan = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 1300px;
+  height: 100%;
+
+  p {
+    font-size: 18px;
+    font-weight: 600;
+    color: ${theme.colors.baseBlue.dark50};
+  }
+
+  .free {
+    background-image: linear-gradient(180deg, ${theme.colors.baseRed.light02} 0%, ${theme.colors.baseRed.dark08} 100%);
+    border: 5px solid ${theme.colors.baseRed.base};
+    border-radius: 16px 0 0 16px;
+    transition: ${transitions.default};
+
+    &:hover {
+      transition: ${transitions.default};
+      transform: translateY(-3px);
+      background-image: linear-gradient(90deg, ${theme.colors.baseRed.light02} 0%, ${theme.colors.baseRed.dark08} 100%);
+    }
+  }
+
+  .freeButton {
+    background-image: linear-gradient(90deg, ${theme.colors.baseRed.light02} 0%, ${theme.colors.baseRed.dark08} 100%);
+    border: 1px solid ${theme.colors.baseRed.base};
+
+    &:hover {
+      background-image: linear-gradient(180deg, ${theme.colors.baseRed.light02} 0%, ${theme.colors.baseRed.dark08} 100%);
+    }
+  }
+
+  .premium {
+    background-image: linear-gradient(180deg, ${theme.colors.baseGreen.light02} 0%, ${theme.colors.baseGreen.dark04} 50%);
+    border: 5px solid ${theme.colors.baseGreen.base};
+    border-radius: 16px;
+    height: 600px;
+
+    &:hover {
+      transition: ${transitions.default};
+      transform: translateY(-3px);
+      background-image: linear-gradient(90deg, ${theme.colors.baseGreen.light02} 0%, ${theme.colors.baseGreen.dark08} 100%);
+    }
+  }
+
+  .premiumButton {
+    background-image: linear-gradient(90deg, ${theme.colors.baseGreen.light02} 0%, ${theme.colors.baseGreen.dark04} 50%);
+    border: 1px solid ${theme.colors.baseGreen.base};
+    border-radius: 16px;
+
+    &:hover {
+      background-image: linear-gradient(180deg, ${theme.colors.baseGreen.light02} 0%, ${theme.colors.baseGreen.dark08} 100%);
+    }
+  }
+
+  .pro {
+    background-image: linear-gradient(180deg, ${theme.colors.baseBlue.light} 0%, ${theme.colors.baseBlue.dark08} 100%);
+    border: 5px solid ${theme.colors.baseBlue.base};
+    border-radius: 0 16px 16px 0;
+
+    &:hover {
+      transition: ${transitions.default};
+      transform: translateY(-3px);
+      background-image: linear-gradient(90deg, ${theme.colors.baseBlue.light02} 0%, ${theme.colors.baseBlue.dark08} 100%);
+    }
+  }
+
+  .proButton {
+    background-image: linear-gradient(90deg, ${theme.colors.baseBlue.light} 0%, ${theme.colors.baseBlue.dark08} 100%);
+    border: 1px solid ${theme.colors.baseBlue.light20};
+
+    &:hover {
+      background-image: linear-gradient(180deg, ${theme.colors.baseBlue.light02} 0%, ${theme.colors.baseBlue.dark08} 100%);
+    }
+  }
+`
+
+export const PricingPlanCard = styled.div`
+  width: 100%;
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+  padding: 16px;
+`
+
+export const PricingCardHeader = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+
+  > div {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+
+    span {
+      padding: 4px 8px;
+      border-radius: 8px;
+      border: 1px solid ${theme.colors.baseGreen.light30};
+      color: ${theme.colors.baseGreen.light30};
+    }
+  }
+
+  h2 {
+    font-size: 32px;
+    margin: 12px 0;
+  }
+`
+
+export const ButtonDiv = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 12px 0;
+
+  button {
+    font-weight: 700;
+    font-size: 18px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 350px;
+    height: 50px;
+    border-radius: 16px;
+  }
+`
+
+export const PricingCardBody = styled.ul`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: space-around;
+
+  li {
+    list-style: none;
+    display: flex;
+    align-items: center;
+    margin-bottom: 12px;
+    gap: 12px;
+
+    svg {
+      color: ${theme.colors.baseGreen.base};
+    }
+  }
+`
