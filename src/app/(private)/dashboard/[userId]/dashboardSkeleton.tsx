7e0bffef5a57f9f3eb80ecfe6c34437294ff5d13
RNT13 <renatornt13@gmail.com>
2025-08-06 12:51:24 -0300

    Update: - Adição de dashBoardSkeleton

--- src/app/(private)/dashboard/[userId]/dashboardSkeleton.tsx
+++ src/app/(private)/dashboard/[userId]/dashboardSkeleton.tsx
@@ -1,24 +1,26 @@
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-import { DashboardColumn, DashboardContent, DashboardHeader } from "./dashboardStyles";
+import { DashboardColumn, DashboardContainer, DashboardContent, DashboardHeader } from "./dashboardStyles";
 
 
 export const DashboardSkeleton = () => {
   return (
-    <DashboardContent className="container">
-      <Skeleton height={600} width={200} />
+    <DashboardContainer className="container">
+      <DashboardContent>
+        <Skeleton height={600} width={200} />
 
-      <DashboardColumn>
-        <Skeleton height={20} width="100%" style={{ marginBottom: 15 }} />
+        <DashboardColumn>
+          <Skeleton height={20} width="100%" style={{ marginBottom: 15 }} />
 
-        <DashboardHeader>
-          <Skeleton width={200} height={150} />
-          <Skeleton width={200} height={150} />
-          <Skeleton width={200} height={150} />
-        </DashboardHeader>
+          <DashboardHeader>
+            <Skeleton width={300} height={150} />
+            <Skeleton width={300} height={150} />
+            <Skeleton width={300} height={150} />
+          </DashboardHeader>
 
-        <Skeleton width='100%' height={395} />
-      </DashboardColumn>
-    </DashboardContent >
+          <Skeleton width='100%' height={395} />
+        </DashboardColumn>
+      </DashboardContent >
+    </DashboardContainer>
   );
 };
